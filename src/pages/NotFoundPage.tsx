a8e3b00a534caa10cc82e351a62d0d460b50d98e
Juliette Ramirez <juliettermz12@gmail.com>
2026-03-30 20:49:47 -0600

    Corrección error duplicado en NotFoundPage

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -13,19 +13,4 @@ export default function NotFoundPage() {
       </button>
     </div>
   );
-}import { useNavigate } from "react-router-dom";
-
-export default function NotFoundPage() {
-  const navigate = useNavigate();
-
-  return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Página no encontrada</h1>
-      <p>La página que buscas no existe o fue eliminada.</p>
-
-      <button onClick={() => navigate("/inicio")}>
-        Regresar al inicio
-      </button>
-    </div>
-  );
 }
